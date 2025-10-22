5af8fe066613ec04884677228b547d9b7f698f25
Patrick Coxall <mr.coxall@mths.ca>
2025-10-22 12:07:22 -0400

    init

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mr. Coxall
+ * Created on: Sep 2025
+ * This program controls a servo.
 */
 
-basic.showString('Hello, World!')
+// variables
+
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+    // move servo to 0 degrees
+})
+
+input.onButtonPressed(Button.B, function () {
+    // move servo to 180 degrees
+})
